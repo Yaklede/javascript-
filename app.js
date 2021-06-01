73af964edf6a3d74c057d7026bd6066a60bd5fcd
Yaklede <chh78151@naver.com>
2021-06-01 12:10:00 +0900

    D-end
    end

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("js-colors");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSavs");
 
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "rgb(44,44,44)";
+const CANVAS_SIZE = 700;
 
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.lineWidth = 2.5;
-ctx.strokeStyle = "rgb(44,44,44)";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+
 let painting = false;
+let filling = false;
 
 function stopPainting(event) {
     painting = false;
@@ -38,16 +48,63 @@ function onMouseDown(event) {
 function handdleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+function handdleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
 }
 
+function handdleModeClick(event){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }
+    else {
+        filling = true;
+        mode.innerText = "PAINT";
+    }
+}
+
+function handdleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+function handdleCM(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+function handdleSaveClick(event){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    link.click();
+}
 
 if(canvas) {
     canvas.addEventListener("mousemove",onMouseMove)
     canvas.addEventListener("mousedown",startPainting)
     canvas.addEventListener("mouseup",stopPainting)
     canvas.addEventListener("mouseleave",stopPainting)
+    canvas.addEventListener("click",handdleCanvasClick)
+    canvas.addEventListener("contextmenu",handdleCM)
 }
 
 Array.from(colors).forEach(color =>
     color.addEventListener("click",handdleColorClick)
 );
+
+if(range){
+    range.addEventListener("input",handdleRangeChange)
+}
+
+if(mode){
+    mode.addEventListener("click",handdleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handdleSaveClick)
+}
